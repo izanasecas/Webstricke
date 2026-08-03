c96f98b9eb5c971475780de2c6ae5719e40002a7
root <root@kali.host>
2026-08-03 13:50:47 +0200

    Adding v.02 webstricke With export output (JSON and TXT), Security headers

--- webstricke.py
+++ webstricke.py
@@ -22,7 +22,7 @@ signal.signal(signal.SIGINT, def_handler)
 
 class WebStrike:
 
-    def __init__(self, url, user_agent, type_attack, quiet, method):
+    def __init__(self, url, user_agent, type_attack, quiet, method, output=None, format='json'):
         
         self.url = url
         if user_agent:
@@ -35,6 +35,9 @@ class WebStrike:
             self.method = method
         else:
             self.method = "GET"
+
+        self.output = output    
+        self.format = format
 
         if not quiet:
             self.banner()
@@ -110,6 +113,71 @@ class WebStrike:
             return module_mapping[self.type_attack]
 
 
+    def export_results(self, results, module_name):
+        # Módulo para la exportación de resultados
+
+        if not self.output:
+            return
+        
+        try:
+            import json
+            from datetime import datetime
+
+            report_data = {
+             "target": self.url,
+             "method": self.method,
+             "module": self.method,
+             "timestamp": datetime.now().isoformat(),
+             "results": results   
+            }
+
+            # Formato JSON
+            if self.format == 'json':
+                with open(self.output, 'w', encoding='utf-8') as f:
+                    json.dump(report_data, f, indent=2, default=str, ensure_ascii=False)
+                print(f"\n[+] Resultados exportdos en {self.output} (JSON)")
+
+            # Formato en TXT
+            elif self.format == 'txt':
+                with open(self.output, 'w', encoding='utf-8') as f:
+                    f.write(f"WebStricke report\n")
+                    f.write(f"{'='*50}\n")
+                    f.write(f"Target: {self.url}\n")
+                    f.write(f"Módulo: {module_name}\n")
+                    f.write(f"Método: {self.method}\n")
+                    f.write(f"Timestamp: {datetime.now().isoformat()}\n")
+                    f.write(f"{'='*50}\n\n")
+
+                    #Información básica
+                    f.write(f"Status code: {results.get('status_code', 'N/A')}\n")
+                    f.write(f"Response time: {results.get('response_time', 'N/A')}s\n")
+                    f.write(f"URL Final: {results.get('final_url', 'N/A')}\n")
+
+                    #Headers
+                    if 'response_headers' in results:
+                        f.write(f"\nHeaders:\n")
+                        for key, value in results['response_headers'].items():
+                            f.write(f"  {key}: {value}")
+                    
+                    # Security headers
+                    if 'security_headers' in results:
+                        f.write(f"\nSecurity Headers:\n")
+                        for header, info in results['security_headers'].items():
+                            value = info.get('value','')
+                            status = info.get('status','UNKNOW')
+                            f.write(f"  {header}: {status}")
+                            if value:
+                                f.write(f"({value})")
+                            f.write(f"\n")
+                    print(f"[+] Resultados exportados a: {self.output} (TXT)")
+        except  Exception as e:
+            print(f"[!] Error ocurrido exportando datos: {e}")
+
+
+
+                    
+
+
     def run(self):
         print("\n[+] Iniciando el proceso")
 
@@ -126,8 +194,10 @@ class WebStrike:
         module_class = self.load_module()
         if module_class:
             module_instance =module_class(target_url=self.url, method=self.method, session=self.session)
-        else:
-            None 
+        
+        if hasattr(module_instance, 'results'):
+            self.scan_results = module_instance.results
+            self.export_results(self.scan_results, self.type_attack) 
 
 
 def main():
@@ -137,10 +207,13 @@ def main():
     parser.add_argument('-t', '--type-attack',help="Type Attack that you want run (SQL, Regex, STTi,...)")
     parser.add_argument('-q', '--quiet', action="store_true", help="Quiet mode(without banner)")
     parser.add_argument('-m', '--method', help="Method of the Requests (GET, POST, PUT...)")
+    parser.add_argument('-o', '--output', help="Archivo de salida, sorporta(txt, json)")
+    parser.add_argument('-f', '--format', choices=['json','txt'], default='json', help='Formato de salida (por defecto JSON)')
+
 
     args = parser.parse_args()
 
-    webstricke = WebStrike(args.url, args.user_agent, args.type_attack, args.quiet, args.method)
+    webstricke = WebStrike(args.url, args.user_agent, args.type_attack, args.quiet, args.method, args.output, args.format )
 
 
 if __name__ == '__main__':
